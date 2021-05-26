0f276eba19b1eeed7f9e00643ee1e01d35bd9ad0
Ioan Latrovaliev <latrovalievioan@gmail.com>
2021-05-26 16:14:58 +0300

    fix: product fetch

--- shopping-cart/src/App.tsx
+++ shopping-cart/src/App.tsx
@@ -21,19 +21,17 @@ function App() {
   React.useEffect(() => {
     fetch("https://cors-anywhere.herokuapp.com/https://ioan-4r7sk1yfwf2q.runkit.sh/products", {
     })
-    .then(response =>{console.log(response)
-    return response})
     .then(response => response.json())
     .then((parsed:Product[]) => {
       setProducts(parsed)
     })
-  })
+  }, [])
 
   return (
     <div className="App">
       <ul>
       {products.map(product => 
-        <li key={product.id}>product</li>
+        <li key={product.id}>{product.name}</li>
       )}
       </ul>
     </div>
