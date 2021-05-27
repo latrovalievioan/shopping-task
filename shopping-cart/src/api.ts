59be3ed1fb2c7bbba24234d9901b4485e5d954c6
Ioan Latrovaliev <latrovalievioan@gmail.com>
2021-05-27 19:55:54 +0300

    feat: implement basket types and api requests

--- shopping-cart/src/api.ts
+++ shopping-cart/src/api.ts
@@ -1,4 +1,5 @@
 import { Product } from "./types"
+import { Basket } from "./types"
 
 const API_URL =
   "https://cors-anywhere.herokuapp.com/https://ioan-4r7sk1yfwf2q.runkit.sh/"
@@ -9,11 +10,36 @@ export const getProducts = (abortController: any): Promise<Product[]> => {
   )
 }
 
-export const getWishlist = (): Promise<Product[]> => {
-  return fetch(`${API_URL}/wishlist`).then((response) => response.json())
+export const getBasket = (abortController: any): Promise<Basket> => {
+  return fetch(`${API_URL}/products`, { signal: abortController.signal }).then(
+    (response) => response.json()
+  )
 }
 
 export const addToBasket = (id: number): Promise<void> =>
   fetch(`${API_URL}/basket/${id}`, { method: "POST" }).then((resp) => {
     if (!resp.ok) return Promise.reject(resp.statusText)
   })
+
+export const removeFromBasket = (id: number): Promise<void> =>
+  fetch(`${API_URL}/basket/${id}`, { method: "DELETE" }).then((resp) => {
+    if (!resp.ok) return Promise.reject(resp.statusText)
+  })
+
+export const increaseItemQuantity = (id: number): Promise<void> =>
+  fetch(`${API_URL}/basket/${id}/increase`, { method: "PATCH" }).then(
+    (resp) => {
+      if (!resp.ok) return Promise.reject(resp.statusText)
+    }
+  )
+
+export const decreaseItemQuantity = (id: number): Promise<void> =>
+  fetch(`${API_URL}/basket/${id}/decrease`, { method: "PATCH" }).then(
+    (resp) => {
+      if (!resp.ok) return Promise.reject(resp.statusText)
+    }
+  )
+
+export const getWishlist = (): Promise<Product[]> => {
+  return fetch(`${API_URL}/wishlist`).then((response) => response.json())
+}
--- shopping-cart/src/types.ts
+++ shopping-cart/src/types.ts
@@ -1,11 +1,17 @@
 export type Product = {
-    id: number;
-    name: string;
-    slug: string;
-    material: string;
-    color: string;
-    description: string;
-    price: string;
-    currency: string;
-    image: string;
-   };
+  id: number
+  name: string
+  slug: string
+  material: string
+  color: string
+  description: string
+  price: string
+  currency: string
+  image: string
+}
+
+export type Basket = {
+  total: number
+  numOfItems: 1
+  items: (Product & { quantity: number })[]
+}
